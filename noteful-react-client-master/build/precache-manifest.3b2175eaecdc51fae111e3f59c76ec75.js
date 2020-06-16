02d116bfed9e2d9815a213dc78c1bdf4de58c857
masonmeier <57198981+masonmeier@users.noreply.github.com>
2020-06-15 18:43:31 -0700

    WE GOT IT WORKING BABY

---
+++ noteful-react-client-master/build/precache-manifest.3b2175eaecdc51fae111e3f59c76ec75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc4979d8c5264f1daef7a935e4407de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62b17ac6584d62698312",
+    "url": "/static/css/main.c4afc636.chunk.css"
+  },
+  {
+    "revision": "eefe86ad8151e7699fb8",
+    "url": "/static/js/2.db0c1bc7.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.db0c1bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b17ac6584d62698312",
+    "url": "/static/js/main.29dcd91c.chunk.js"
+  },
+  {
+    "revision": "9f4911f97d91ab5a4bb7",
+    "url": "/static/js/runtime-main.2ed5e891.js"
+  }
+]);
